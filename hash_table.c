1ca45327c69f093af2c50b1efea3e21696ea06ee
Zachary Forman <au.zachary.forman@gmail.com>
2014-04-15 22:57:13 +0930

    Updated!

--- hash_table.c
+++ hash_table.c
@@ -2,17 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned long hash_str(char *str)
-{
-    unsigned long hash = 5381;
-    int c;
-
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + (unsigned)c; /* hash * 33 + c */
-
-    return hash;
-}
-
 typedef char *key_t;
 typedef int val_t;
 
@@ -40,9 +29,13 @@ typedef struct hash_table
     unsigned long sz;
     unsigned long (*hash_func)(key_t);
     int (*compare)(key_t, key_t);
+    void (*key_free)(key_t);
+    void (*val_free)(val_t);
 } map_t;
 
-map_t *make_map(unsigned long sz, unsigned long (*hash_func)(key_t), int (*compare)(key_t, key_t))
+map_t *make_map(unsigned long sz, unsigned long (*hash_func)(key_t), 
+                int (*compare)(key_t, key_t), void (*key_free)(key_t),
+                void (*val_free)(val_t))
 {
     int i = 0;
     map_t *ret;
@@ -54,6 +47,8 @@ map_t *make_map(unsigned long sz, unsigned long (*hash_func)(key_t), int (*compa
         ret->d[i] = NULL;
     ret->hash_func = hash_func;
     ret->compare = compare;
+    ret->key_free = key_free;
+    ret->val_free = val_free;
     return ret;
 }
 
@@ -115,7 +110,8 @@ void delete(map_t *m, const key_t k)
         return;
     }
     if (m->compare(m->d[pos]->key, k) == 0) {
-        free(m->d[pos]->key);
+        m->key_free(m->d[pos]->key);
+        m->val_free(m->d[pos]->val);
         free(m->d[pos]);
         m->d[pos] = NULL;
     }
@@ -124,7 +120,8 @@ void delete(map_t *m, const key_t k)
         if(bkt == NULL) return;
         if(m->compare(k, bkt->key) == 0) {
             prev->next = bkt->next;
-            free(bkt->key);
+            m->key_free(bkt->key);
+            m->val_free(bkt->val);
             free(bkt);
             return;
         }
@@ -133,25 +130,68 @@ void delete(map_t *m, const key_t k)
     }
 }
 
-int mystrcmp(char* a, char* b)
+
+/* Funcs used for init */
+
+unsigned long hash_str(char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + (unsigned)c; /* hash * 33 + c */
+
+    return hash;
+}
+
+int mystrcmp(char *a, char *b)
 {
     return strcmp(a,b);
 }
 
+void my_str_free(char *a)
+{
+    free(a);
+}
+
+void my_int_free(int a)
+{
+}
+
+/* cool little map thing */
 int main()
 {
-    map_t *m = make_map(1024, hash_str, mystrcmp);
-    store(m, strdup("hello"), 1);
-    store(m, strdup("world"), 2);
-    printf("m[hello]: %d\n", *get(m, "hello"));
-    printf("m[world]: %d\n", *get(m, "world"));
-    printf("&m[foo]:  %p\n\n", get(m, "foo"));
-    delete(m, "hello");
-    printf("m[hello]: %p\n", get(m, "hello"));
-    printf("m[world]: %d\n", *get(m, "world"));
-    printf("&m[foo]:  %p\n\n", get(m, "foo"));
-    store(m, "world", 100);
-    printf("m[hello]: %p\n", get(m, "hello"));
-    printf("m[world]: %d\n", *get(m, "world"));
-    printf("&m[foo]:  %p\n\n", get(m, "foo"));
+    int i;
+    char c;
+    char key[1024];
+    int val;
+    int *v;
+    for (i = 0; i < 1024; i++)
+        key[i] = 0;
+    map_t *m = make_map(1024, hash_str, mystrcmp, my_str_free, my_int_free);
+    for(;;) {
+        c = 0;
+        scanf("%c", &c);
+        switch(c) {
+        case 'q': return;
+        case 's': 
+            scanf("%s %d", key, &val);
+            store(m, key, val);
+        break;
+        case 'r': 
+            scanf("%s", key);
+            v = get(m, key);
+            if(v)
+                printf("m[%s] == %d\n", key, *v);
+            else 
+                printf("m[%s] not stored\n");
+        break;
+        case 'd': 
+            delete(m, key);
+            printf("m[%s] removed\n", key);
+        break;
+        default:
+        break;
+        }
+    }
 }
