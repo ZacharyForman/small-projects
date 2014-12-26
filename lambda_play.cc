66452a34d49e5ff98ea525be70ad904c00e0df1a
Zachary Forman <au.zachary.forman@gmail.com>
2014-12-26 23:24:51 +1030

    Fixed a problem and added another two example usages.

--- lambda_play.cc
+++ lambda_play.cc
@@ -2,7 +2,6 @@
 
 int main()
 {
-namespace{
   // basics
   int x = 5;
 
@@ -22,9 +21,26 @@ namespace{
   printf("f1(5) = %d\n", f1(5));
   printf("f2(3) = %d\n", f2(3));
   printf("f3(1) = %d\n", f3(1));
-}
-namespace{
-  // stack smashing
 
-}
+  int y = 5;
+  // mutable required to modify it locally.
+  auto f5 = [=](int a) mutable { 
+    printf("%d\n", y); // y
+    y = a;
+    printf("%d\n", y); // y == a
+  };
+  f5(1);
+  printf("%d\n", y);
+
+  auto f6 = [](int a) {
+    int x = a;
+    auto f7 = [&]() { return x; };
+    return f7;
+  };
+
+  auto f7 = f6(11);
+  printf("%d\n", f7()); // 11!
+  printf("%d\n", f7()); // Garbage!
+  printf("%d\n", f7()); // Garbage!
+
 }
